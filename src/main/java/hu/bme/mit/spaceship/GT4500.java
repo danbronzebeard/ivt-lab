02a50c55ab846a2f314b21de7e59a812bf427b78
danbronzebeard <danimolnar2@gmail.com>
2024-04-26 10:55:51 +0200

    fix: redundant TODO comment deleted without deleting other comment

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -77,6 +77,7 @@ public class GT4500 implements SpaceShip {
         break;
 
       case ALL:
+        // try to fire both of the torpedo stores
         boolean successSec = false;
         boolean successPrim = false;
 
